013cc2a881e7d4d383d1fbde1172ca8a143e0159
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-29 12:27:38 +0900

    とりあえずできた!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 const app = Vue.createApp({
-    data:() =>({
-        message:'instance1'
-    })
+    data() {
+        return {
+            newItem:"",
+            todos:[],
+            alertMessage:""
+        }
+    },
+    methods: {
+        addItem: function(){
+            if (this.newItem === '') {
+                this.alertMessage = "required"
+                return
+            }
+            let todo = {
+                item  :this.newItem,
+                isDone:false,
+            }
+            this.todos.push(todo)
+            this.newItem = ""
+            this.alertMessage = ""
+        },
+        deleteItem:function (index) {
+            this.todos.splice(index,1)//削除を始める場所､個数
+        }
+    },
+
 })
 app.mount('#app')
 
